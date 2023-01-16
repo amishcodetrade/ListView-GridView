8f67873a451ea078db173d6e161171ae8c6e1eda
amishcodetrade <amish.kajavadra@mail.codetrade.io>
2023-01-16 18:37:59 +0530

    Initial commit

--- lib/ListView/listviewseparated.dart
+++ lib/ListView/listviewseparated.dart
@@ -28,6 +28,75 @@ class Listviewseparated extends StatelessWidget {
       'kenal',
       'bholu'
     ];
+    var city = [
+      'surat',
+      'rajkot',
+      'mumbai',
+      'delhi',
+      'delhi',
+      'vadodra',
+      'jamnagar',
+      'vadodra',
+      'suranagar',
+      'rajkot',
+      'junagath',
+      'surat',
+      'surat',
+      'jamnagar',
+      'jamnagar',
+      'vadodra',
+      'capital',
+      'rajkot',
+      'bharuch',
+      'navsari',
+      'dang'
+    ];
+    var year = [
+      '2019',
+      '2018',
+      '2017',
+      '2016',
+      '2015',
+      '2001',
+      '2000',
+      '2001',
+      '2188',
+      '2100',
+      '1999',
+      '1998',
+      '1997',
+      '2017',
+      '2016',
+      '2015',
+      '2014',
+      '2012',
+      '2011',
+      '2010',
+      '2009'
+    ];
+    List<String> images = [
+      "assets/male.jpg",
+      "assets/m1.png",
+      "assets/m2.jpeg",
+      "assets/m3.jpg",
+      "assets/m4.png",
+      "assets/male.jpg",
+      "assets/m1.png",
+      "assets/m3.jpg",
+      "assets/m2.jpeg",
+      "assets/m4.png",
+      "assets/male.jpg",
+      "assets/m1.png",
+      "assets/m2.jpeg",
+      "assets/m3.jpg",
+      "assets/m4.png",
+      "assets/male.jpg",
+      "assets/m1.png",
+      "assets/m3.jpg",
+      "assets/m2.jpeg",
+      "assets/m4.png",
+      "assets/m4.png",
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('ListView Separated'),
@@ -36,26 +105,33 @@ class Listviewseparated extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  name[index],
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+                SizedBox(
+                    width: 80, height: 60, child: Image.asset(images[index])),
+                Column(
+                  children: [
+                    Text(
+                      name[index],
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      city[index],
+                      style: const TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                  ],
                 ),
                 Text(
-                  name[index],
+                  year[index],
                   style: const TextStyle(
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
                       color: Colors.red),
-                ),
-                Text(
-                  name[index],
-                  style: const TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue),
                 ),
               ],
             ),
